7a73a439dd9224fed400ecfec44128f527a8ce42
renchaokang <renchaokang@cambricon.com>
2023-11-17 12:00:31 +0800

    update

--- beike/plot_2.py
+++ beike/plot_2.py
@@ -6,7 +6,7 @@ newest_day = datetime.datetime(2023, 10, 31)
 
 
 def fliter(datas):
-    size = 3
+    size = 5
     half_size = round(size/2)
     lens = len(datas)
     data_meaning = []
@@ -26,74 +26,113 @@ def fliter(datas):
     return data_meaning
 
 
-with open('data/qilinkechuangyuan.txt') as lines:  # 一次性读入txt文件，并把内容放在变量lines中
-    array = lines.readlines()  # 返回的是一个列表，该列表每一个元素是txt文件的每一行
-
-t = 0
-deal_dict = {}
-for ite in array:
-    tiny = ite.split(',')
-    msg = tiny[0].split(':')[1]
-    name = msg.split(' ')[1]
-    name = name.split('"')[1]
-    sqr = msg.split(' ')[3]
-    sqr_digit = float(re.findall(r"\d+\.?\d*", sqr)[0])
-    digits = [sqr_digit]
-    for i in range(5):
-        strs = tiny[i+1]
-        if(i == 2):
-            ymd = strs.split('"')[3].split('.')
-            day = datetime.datetime(int(ymd[0]), int(ymd[1]), int(ymd[2]))
-            delta = newest_day - day
-            digits.append(delta.days)
+def get_dict(plate_name):
+    with open(plate_name) as lines:  # 一次性读入txt文件，并把内容放在变量lines中
+        array = lines.readlines()  # 返回的是一个列表，该列表每一个元素是txt文件的每一行
+    t = 0
+    plate_dict = {}
+    for ite in array:
+        tiny = ite.split(',')
+        msg = tiny[0].split(':')[1]
+        name = msg.split(' ')[1]
+        name = name.split('"')[1]
+        sqr = msg.split(' ')[3]
+        sqr_digit = float(re.findall(r"\d+\.?\d*", sqr)[0])
+        digits = [sqr_digit]
+        for i in range(5):
+            strs = tiny[i+1]
+            if(i == 2):
+                ymd = strs.split('"')[3].split('.')
+                day = datetime.datetime(int(ymd[0]), int(ymd[1]), int(ymd[2]))
+                # delta = newest_day - day
+                # digits.append(delta.days)
+                digits.append(day)
+            else:
+                digit_list = re.findall(r"\d+\.?\d*", strs)
+                digit = "0"
+                if digit_list != []:
+                    digit = digit_list[0]
+                digits.append(float(digit))
+        if name in plate_dict:
+            plate_dict[name].append(digits)
         else:
-            digit_list = re.findall(r"\d+\.?\d*", strs)
-            digit = "0"
-            if digit_list != []:
-                digit = digit_list[0]
-            digits.append(float(digit))
-    if name in deal_dict:
-        deal_dict[name].append(digits)
-    else:
-        deal_dict[name] = [digits]
+            plate_dict[name] = [digits]
 
-    t += 1
-    # if(t > 2):
-    #     break
-data = deal_dict["中海国际社区一期"]
-print(len(data))
-price = []
-day = []
-sqr = []
-for ite in data:
-    sqr = ite[0]
-    if(sqr > 85 and sqr < 100):
-        price.append(ite[2])
-        day.append(ite[3])
-    # price.append(ite[2])
-    # day.append(ite[3])
-last_day = day[-1]
-print(last_day)
-days = []
-for ite in day:
-    dd = - ite + last_day
-    days.append(dd)
+        t += 1
 
-# price = fliter(price)
-# day.reverse()
-plt.plot(days, price)
+        # if(t > 2):
+        #     break
+    return plate_dict
 
-half_year = 182.5
-num1 = 2020
-num2 = 1
-for i in range(6):
-    if(i % 2 == 0):
-        num1 += 1
-        num2 = 1
-    else:
-        num2 = 7
-    note = str(num1) + ".0" + str(num2)
-    plt.axvline(half_year * i - 31, color='r', linestyle=':')
-    plt.text(half_year * i - 31, 24000, note, fontdict={
-        'family': 'serif', 'size': 16, 'color': 'blue'}, ha='center', va='center')
-plt.show()
+
+def plot_community_price(plate_dict, community_name):
+    data = plate_dict[community_name]
+
+    price = []
+    days = []
+    sqr = []
+    for ite in data:
+        sqr = ite[0]
+        if(sqr > 75 and sqr < 115):
+            price.append(ite[2])
+            days.append(ite[3])
+
+        # price.append(ite[2])
+        # days.append(ite[3])
+
+    day_len = len(days)
+    day_list = []
+    for i in range(day_len):
+        if i == 0:
+            day_list.append(0)
+        else:
+            delta = (days[i] - days[0]).days
+            day_list.append(delta)
+    add_day = -day_list[-1]
+    print(add_day)
+    for i in range(day_len):
+        day_list[i] += add_day
+
+    year = days[-1].year
+    mon = days[-1].month
+    n_year = days[0].year
+    n_mon = days[0].month
+    # print(year, mon)
+    # print(n_year, n_mon)
+    # print((data[0][3] - data[-1][3]).days)
+
+    note_list = []
+
+    ite_year = year
+    while ite_year <= n_year:
+        gap_day = (datetime.datetime(ite_year, 1, 1) - days[-1]).days
+        gap_str = str(ite_year % 100) + '.1'
+        note_list.append([gap_day, gap_str])
+        gap_day = (datetime.datetime(ite_year, 7, 1) - days[-1]).days
+        gap_str = str(ite_year % 100) + '.7'
+        note_list.append([gap_day, gap_str])
+        ite_year += 1
+    print(note_list)
+    price = fliter(price)
+    plt.plot(day_list, price)
+
+    level = min(price)
+    for note in note_list:
+        plt.axvline(note[0], color='r', linestyle=':')
+        plt.text(note[0], level, note[1], fontdict={
+            'family': 'serif', 'size': 16, 'color': 'blue'}, ha='center', va='center')
+        mon += 6
+        if mon > 12:
+            mon -= 12
+            year += 1
+        i += 1
+    plt.show()
+
+
+if __name__ == '__main__':
+    # 抓取数据 参数一：总页数，参数二：区县，可选、不传默认全部
+    plate_name = 'data/qilinkechuangyuan.txt'
+    # plate_name = 'data/wanshou1.txt'
+    plate_dict = get_dict(plate_name)
+    community_name = "富力城"
+    plot_community_price(plate_dict, community_name)
